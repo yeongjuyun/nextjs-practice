9e9176c6d474b40080e51b67473ee536aa86776a
yeongjuyun <myyun1029@gmail.com>
2022-06-28 11:04:26 +0900

    exp 1

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div>Hello nextsdfsdfjs</div>;
+  return <div>Hello nextjs - exp</div>;
 }
